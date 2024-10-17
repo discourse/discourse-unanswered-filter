a0e0d69f5dad505d136ee338ff4cf365d7a90596
Kelv <kelv@discourse.org>
2024-10-17 17:33:57 +0800

    DEV: Update linting setup (#32)

--- spec/system/logged_out_link_spec.rb
+++ spec/system/logged_out_link_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe "Unanswered Filter Component - logged-out link test",
-               system: true do
+RSpec.describe "Unanswered Filter Component - logged-out link test", system: true do
   let!(:theme) { upload_theme_component }
 
   fab!(:category)
